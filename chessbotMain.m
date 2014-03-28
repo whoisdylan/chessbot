4edf4a86870ac6f2d248c9b9e09ead65f01dcd8e
Dylan Koenig <koenigup@gmail.com>
2014-03-27 22:36:12 -0400

    implemented fsm for main to deal with when player is moving and when board state is valid, added hand detection to getBoard

--- chessbotMain.m
+++ chessbotMain.m
@@ -13,34 +13,55 @@ board(3:6,:) = repmat({'empty'},4,8);
 
 gameOver = false;
 transferFunction = initBoard();
+%I don't know how to enum in matlab so states: wait=0, playerMoving=1,
+%playerDone=2
+currState = 0;
 while (~gameOver)
-    currentBoardImage = getBoard(transferFunction);
-    if (whiteMoved)
-        newBoardImage = getBoard(transferFunction);
-        changeList = scanBoardForChanges(currentBoardImage, newBoardImage);
-        for i=1:2:size(changeList,1)/2
-            oldRow = changeList(i,1);
-            oldCol = changeList(i,2);
-            newRow = changeList(i+1,1);
-            newCol = changeList(i+1,2);
-            movedPiece = board(oldRow, oldCol);
-            %check if piece was captured
-            displacedPiece = board(newRow, newCol);
-            board(newRow, newCol) = movedPiece;
-            board(oldRow, oldCol) = 'empty';
-            %send movement commands to other chessbot
-            if (~strcmp(displacedPiece,'empty'))
-                removePieceFromBoard(newRow, newCol);
+    switch currState
+        %waiting for player to move
+        case 0
+            [currBoard, handPresent] = getBoard(transferFunction);
+            %if hand is present, player is moving so don't update board
+            if (handPresent)
+                nextState = 1;
+            else
+                prevBoard = currBoard;
             end
-            movePiece(oldRow, oldCol, newRow, newCol);
-            %check for special cases
-            if (newRow == 8 && strcmp(movedPiece, 'pawn'))
-                %if pawn reaches the end, replace with queen
-                board(newRow, newCol) = 'queen';
-                replacePawnWithQueen(newRow, newCol);
+        %player is moving
+        case 1
+            [currBoard, handPresent] = getBoard(transferFunction);
+            %if there's no hand, then the player is done moving so grab the
+            %board
+            if (~handPresent)
+                nextState = 2;
+                nextBoard = currBoard;
             end
-        end
-    elseif (myTurn)
-        %accept commands from other chessbot to perform movements
+        %player done moving
+        case 2
+            changeList = scanBoardForChanges(prevBoard, nextBoard);
+            for i=1:2:size(changeList,1)/2
+                oldRow = changeList(i,1);
+                oldCol = changeList(i,2);
+                newRow = changeList(i+1,1);
+                newCol = changeList(i+1,2);
+                movedPiece = board(oldRow, oldCol);
+                %check if piece was captured
+                displacedPiece = board(newRow, newCol);
+                board(newRow, newCol) = movedPiece;
+                board(oldRow, oldCol) = 'empty';
+                %send movement commands to other chessbot
+                if (~strcmp(displacedPiece,'empty'))
+                    removePieceFromBoard(newRow, newCol);
+                end
+                movePiece(oldRow, oldCol, newRow, newCol);
+                %check for special cases
+                if (newRow == 8 && strcmp(movedPiece, 'pawn'))
+                    %if pawn reaches the end, replace with queen
+                    board(newRow, newCol) = 'queen';
+                    replacePawnWithQueen(newRow, newCol);
+                end
+            end
+            nextState = 0;
     end
+    currState = nextState;
 end
--- kinect_for_windows_SDK/getBoard.m
+++ kinect_for_windows_SDK/getBoard.m
@@ -1,4 +1,4 @@
-function [ boardState ] = getBoard( transferFunction)
+function [ boardState, handPresent ] = getBoard( transferFunction)
     % Requires tqf to be calculated via initBoard (transformation)
 
     [colorImage,depthImage] = getFrame();
@@ -13,23 +13,28 @@ function [ boardState ] = getBoard( transferFunction)
     
     hold on, h=imagesc(depthImage);
     rgn = round(length(depthImage)/8);
-    thresh = 100;
+    pieceThresh = 100;
+    handThresh = 2000;
     boardState = zeros(8);
+    boardMin = min(depthImage(:));
+    handPresent = false;
     for row=1:8
         for col=1:8
             region = depthImage(((row-1)*rgn+1):row*rgn,((col-1)*rgn+1):col*rgn);
             region(region == 0) = NaN;
-            mindep = min(region(:));
-            maxdep = max(region(:));
-            
-            if(isnan(mindep) || isnan(maxdep)) 
+            minDepth = min(region(:));
+            maxDepth = max(region(:));
+            %return if hand is on board, player is moving
+            if ((maxDepth - boardMin) > handThresh)
+                handPresent = true;
+                return;
+            end
+            if(isnan(minDepth) || isnan(maxDepth)) 
                 disp 'all NaNs in region '
                 [row,col]
-                
                 continue;
             end
-            boardState(row,col) = maxdep - mindep > thresh;
-            
+            boardState(row,col) = maxDepth - minDepth > pieceThresh;
         end
     end
     
